11737ebb87801354469b58d8839a317cf73ca008
gastonframirez <gastonframirez@gmail.com>
2018-05-02 01:17:25 -0300

    Presup. Participativo + llamadas APIv4 + Estructura + cookies

--- assets/js/participativoGrapher.js
+++ assets/js/participativoGrapher.js
@@ -1,39 +1,40 @@
 window.already_printed_participativo = false;
 
 function dibujarD3_participativo() {
-  $.getJSON("https://spreadsheets.google.com/feeds/list/1uA9UbORQPEpzDVBpvq4UsmiA0eOvAdFCMg31iXd1XoE/or4ki2f/public/values?alt=json", function( dataJSON ) {
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1O2Dk6ndwUnfOXE-YKMvSNuprFCpaMDaHz5OUAdxNLNw/values/"+selectedYear+"?key=AIzaSyDWqm99ehcgTUcnekuujkT2P95l-kor_mM", function( dataJSON ) {
     var datos = [];
     var detalle = []
     var $tabla = $("#tbody-participativo");
     $("#participativoGraph").empty();
-    $.each( dataJSON.feed.entry, function( key, val ) {
+    $.each( dataJSON.values, function( key, val ) {
+    	if(key>0){
+    	      var concepto = val[1];
+    	      var porcentaje = val[2]
+    	      var nivel_tabla = val[0];
+    	      var monto = val[3];
+    	      var nivel_tabla_splited = nivel_tabla.split('.');
+    	      var nivel = nivel_tabla_splited.length + 1;
+    	      var texto = val[4];
+    	      detalle[nivel] = concepto.toLowerCase().split(' ').join('_');
 
-      var concepto = val.gsx$programa.$t;
-      var porcentaje = val.gsx$porcentaje.$t;
-      var nivel_tabla = val.gsx$nivel.$t;
-      var monto = val.gsx$monto.$t;
-      var nivel_tabla_splited = nivel_tabla.split('.');
-      var nivel = nivel_tabla_splited.length + 1;
-      var texto = val.gsx$texto.$t;
-      detalle[nivel] = concepto.toLowerCase().split(' ').join('_');
+    	      if (nivel_tabla.length == 1 && nivel_tabla != 9) {
+    	        datos.push({
+    	          "key": concepto.substr(14,concepto.length-1),
+    	          "valor": parseInt(val[3].split(".").join(""))
+    	        });
+    	      }
 
-      if (nivel_tabla.length == 1 && nivel_tabla != 9) {
-        datos.push({
-          "key": concepto.substr(14,concepto.length-1),
-          "valor": parseInt(val.gsx$monto.$t.split(".").join(""))
-        });
-      }
-
-      if(nivel_tabla == "9"){
-        $tabla.append('<tr class="nivel-1"><td></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
-      }else{
-        if(nivel == 3){
-          $tabla.append('<tr class="table-clickable nivel-3" data-toggle="collapse" data-target="#texto-'+detalle[2]+'-'+detalle[3]+'"><td><button class="btn btn-xs btn-default pull-right"><i class="fa fa-plus "></i></button></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
-          $tabla.append('<tr class="collapse gray" id="texto-'+detalle[2]+'-'+detalle[3]+'"><td></td><td colspan="3">'+texto+'</td></tr>');
-        }else{
-          $tabla.append('<tr class="nivel-'+nivel+'"><td></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
-        }
-      }
+    	      if(nivel_tabla == "9"){
+    	        $tabla.append('<tr class="nivel-1"><td></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
+    	      }else{
+    	        if(nivel == 3){
+    	          $tabla.append('<tr class="table-clickable nivel-3" data-toggle="collapse" data-target="#texto-'+detalle[2]+'-'+detalle[3]+'"><td><button class="btn btn-xs btn-default pull-right"><i class="fa fa-plus "></i></button></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
+    	          $tabla.append('<tr class="collapse gray" id="texto-'+detalle[2]+'-'+detalle[3]+'"><td></td><td colspan="3">'+texto+'</td></tr>');
+    	        }else{
+    	          $tabla.append('<tr class="nivel-'+nivel+'"><td></td><td>'+concepto+'</td><td>'+porcentaje+'</td><td>$'+monto.toLocaleString("es-AR")+'</td></tr>');
+    	        }
+    	      }
+    	}
     });
     console.log(datos);
     var visualization = d3plus.viz()
