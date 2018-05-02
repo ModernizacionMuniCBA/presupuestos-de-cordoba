3cbc43ce905a8608b27a6fa4420097e990729b36
gastonframirez <gastonframirez@gmail.com>
2018-05-02 01:07:30 -0300

    Llamadas APIv4 + Estructura en Gastos + cookies

--- assets/js/gastosGrapher.js
+++ assets/js/gastosGrapher.js
@@ -14,7 +14,7 @@ function dibujarD3_gastos() {
   }
   dibujarD3_gastos_finalidad();
   dibujarD3_gastos_finalidad_funcion();
-  $.getJSON("https://spreadsheets.google.com/feeds/list/11UGXTNvmaOezQNpFXxMXps_gT7kwEP08YeMdcz4wKXo/od6/public/values?alt=json", function( dataJSON ) {
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/11UGXTNvmaOezQNpFXxMXps_gT7kwEP08YeMdcz4wKXo/values/"+selectedYear+"?key=AIzaSyDWqm99ehcgTUcnekuujkT2P95l-kor_mM", function( dataJSON ) {
   $("#clasificacion-economica-gasto").empty();
     var datos = [];
     var datos_clasif_econo = [];
@@ -22,53 +22,55 @@ function dibujarD3_gastos() {
     var $tabla = $("#tbody-clasificacion-gasto");
     // console.log(dataJSON.feed.entry);
     var i = 0;
-    $.each( dataJSON.feed.entry, function( key, val ) {
-      i += 1;
-      var partida = val.gsx$partida.$t;
-      var nivel_tabla = val.gsx$nivel.$t;
-      var partida_splited = partida.split('.');
-      var nivel = partida_splited.length;
-      var nivel_princ = parseInt(partida_splited[0]);
-      var subnivel = parseInt(partida_splited[1]);
-      var concepto = val.gsx$concepto.$t;
-      var total = val.gsx$total.$t;
-      if(nivel_tabla != -2){
-        if (partida_splited[1]==""){
-          nivel -=1;
-        }
-        detalle[nivel] = concepto;
+    $.each( dataJSON.values, function( key, val ) {
+    	if(key>0){
+    		   i += 1;
+    		      var partida = val[1];
+    		      var nivel_tabla = val[0];
+    		      var partida_splited = partida.split('.');
+    		      var nivel = partida_splited.length;
+    		      var nivel_princ = parseInt(partida_splited[0]);
+    		      var subnivel = parseInt(partida_splited[1]);
+    		      var concepto = val[2];
+    		      var total = val[3];
+    		      if(nivel_tabla != -2){
+    		        if (partida_splited[1]==""){
+    		          nivel -=1;
+    		        }
+    		        detalle[nivel] = concepto;
 
-        if(nivel == 3 && nivel_princ==2 && subnivel==4){
-          nivel = 4;
-          detalle[3] = concepto
-        }
-        if(nivel == 4){
+    		        if(nivel == 3 && nivel_princ==2 && subnivel==4){
+    		          nivel = 4;
+    		          detalle[3] = concepto
+    		        }
+    		        if(nivel == 4){
 
-            var linea = {  "key": concepto,
-                          "rec1": detalle[3],
-                          "rec2": detalle[2],
-                          "rec3": detalle[1],
-                         "valor": parseInt(total.split('.').join(""))}
+    		            var linea = {  "key": concepto,
+    		                          "rec1": detalle[3],
+    		                          "rec2": detalle[2],
+    		                          "rec3": detalle[1],
+    		                         "valor": parseInt(total.split('.').join(""))}
 
-            datos.push(linea);
-        }
-        if(already_printed_clasificacion == false){
-          if(nivel <=4){
-            if(partida=="-2"){
-              $tabla.append('<tr class="nivel-'+nivel+' total"><td></td><th scope="row"> </th><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-            }else{
-              var mostrar = nivel == 4 ? " style='display:none'":'';
-              var plus = nivel == 3 ? ' <button class="btn btn-xs btn-default pull-right"><i class="fa fa-plus "><i></button>' : '';
-              if(i>1){
-                $tabla.append('<tr'+mostrar+' class="nivel-'+nivel+'"><td>'+plus+'</td><th scope="row">'+partida+'</th><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-              }else{
-                $tabla.append('<tr'+mostrar+' class="nivel-1"><td>'+plus+'</td><th scope="row">'+partida+'</th><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-              }
-            }
+    		            datos.push(linea);
+    		        }
+    		        if(already_printed_clasificacion == false){
+    		          if(nivel <=4){
+    		            if(partida=="-2"){
+    		              $tabla.append('<tr class="nivel-'+nivel+' total"><td></td><th scope="row"> </th><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    		            }else{
+    		              var mostrar = nivel == 4 ? " style='display:none'":'';
+    		              var plus = nivel == 3 ? ' <button class="btn btn-xs btn-default pull-right"><i class="fa fa-plus "><i></button>' : '';
+    		              if(i>1){
+    		                $tabla.append('<tr'+mostrar+' class="nivel-'+nivel+'"><td>'+plus+'</td><th scope="row">'+partida+'</th><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    		              }else{
+    		                $tabla.append('<tr'+mostrar+' class="nivel-1"><td>'+plus+'</td><th scope="row">'+partida+'</th><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    		              }
+    		            }
 
-          }
-        }
-      }
+    		          }
+    		        }
+    		      }
+    	}
     });
 
     //console.log(datos);
@@ -108,7 +110,7 @@ function dibujarD3_gastos() {
   });
 }
 function dibujarD3_gastos_corrientes() {
-  $.getJSON("https://spreadsheets.google.com/feeds/list/12aHIOCmpAYYOx2mBoO_IzAqfjeOUX0AnX7BheISd4go/od6/public/values?alt=json", function( dataJSON ) {
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/12aHIOCmpAYYOx2mBoO_IzAqfjeOUX0AnX7BheISd4go/values/"+selectedYear+"?key=AIzaSyDWqm99ehcgTUcnekuujkT2P95l-kor_mM", function( dataJSON ) {
     var datos = [];
     var datos_clasif_econo = [];
     var detalle = [];
@@ -117,96 +119,101 @@ function dibujarD3_gastos_corrientes() {
     var regexDosNumeros = /[0-9]{2}/;
     // console.log(dataJSON.feed.entry);
     var i = 0;
-    $.each( dataJSON.feed.entry, function( key, val ) {
-      i += 1;
-      var partida = val.gsx$partida.$t;
-      var partida_splited = partida.split('.');
-      var nivel = partida_splited.length;
-      var nivel_princ = parseInt(partida_splited[0]);
-      var concepto = val.gsx$concepto.$t;
-      var total = val.gsx$total.$t;
-      if (partida_splited[1]==""){
-        nivel -=1;
-      }
-      detalle[nivel] = concepto;
+    $.each( dataJSON.values, function( key, val ) {
+    	if(key>0){
+    		i += 1;
+    	      var partida = val[0];
+    	      var partida_splited = partida.split('.');
+    	      var nivel = partida_splited.length;
+    	      var nivel_princ = parseInt(partida_splited[0]);
+    	      var concepto = val[1];
+    	      var total = val[2];
+    	      if (partida_splited[1]==""){
+    	        nivel -=1;
+    	      }
+    	      detalle[nivel] = concepto;
 
-      if (partida_splited[0] == "-1" && i>1){
-        if(concepto.toUpperCase() != "EROGACIONES DE CAPITAL"){
-          $tabla.append('<tr class="nivel-1"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-        }
-      }
-      if (partida_splited[0] == "0"){
-        $tabla.append('<tr class="nivel-2"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-      } else if (nivel_princ ==  1) {
-        if(regexDosNumeros.test(concepto.substr(0,2))) {
-          concepto = concepto.substr(2,concepto.length-1);
-        }
-        $tabla.append('<tr class="nivel-3"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-      }
+    	      if (partida_splited[0] == "-1" && i>1){
+    	        if(concepto.toUpperCase() != "EROGACIONES DE CAPITAL"){
+    	          $tabla.append('<tr class="nivel-1"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	        }
+    	      }
+    	      if (partida_splited[0] == "0"){
+    	        $tabla.append('<tr class="nivel-2"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	      } else if (nivel_princ ==  1) {
+    	        if(regexDosNumeros.test(concepto.substr(0,2))) {
+    	          concepto = concepto.substr(2,concepto.length-1);
+    	        }
+    	        $tabla.append('<tr class="nivel-3"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	      }
+    	}
+      
     });
     already_printed_corrientes=true;
   });
 }
 
 function dibujarD3_gastos_capital() {
-  $.getJSON("https://spreadsheets.google.com/feeds/list/1dHvfU13hR8rThdLqplWKSMg6t8Xt3oq1nxB9c-ZJlfA/od6/public/values?alt=json", function( dataJSON ) {
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1dHvfU13hR8rThdLqplWKSMg6t8Xt3oq1nxB9c-ZJlfA/values/"+selectedYear+"?key=AIzaSyDWqm99ehcgTUcnekuujkT2P95l-kor_mM", function( dataJSON ) {
     var datos = [];
     var datos_clasif_econo = [];
     var detalle = [];
     var i = 0;
-    $.each( dataJSON.feed.entry, function( key, val ) {
-      i += 1;
-      var partida = val.gsx$partida.$t;
-      var partida_splited = partida.split('.');
-      var nivel = partida_splited.length;
-      var nivel_princ = parseInt(partida_splited[0]);
-      var concepto = val.gsx$concepto.$t;
-      var total = val.gsx$total.$t;
-      if (partida_splited[1]==""){
-        nivel -=1;
-      }
-      detalle[nivel] = concepto;
+    $.each( dataJSON.values, function( key, val ) {
+    	if(key>0){
+    		 i += 1;
+    	      var partida = val[0];
+    	      var partida_splited = partida.split('.');
+    	      var nivel = partida_splited.length;
+    	      var nivel_princ = parseInt(partida_splited[0]);
+    	      var concepto = val[1];
+    	      var total = val[2];
+    	      if (partida_splited[1]==""){
+    	        nivel -=1;
+    	      }
+    	      detalle[nivel] = concepto;
 
-      if (partida_splited[0] == "-1" && i>1){
-        if(concepto.toUpperCase() != "EROGACIONES CORRIENTES"){
-          $("#tbody-gastos-capital").append('<tr class="nivel-1"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-        }
-      }
-      if (partida_splited[0] == "0"){
-        $("#tbody-gastos-capital").append('<tr class="nivel-2"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-      }
-      if (partida_splited[0] == "1"){
-        $("#tbody-gastos-capital").append('<tr class="nivel-3"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-      }
-
+    	      if (partida_splited[0] == "-1" && i>1){
+    	        if(concepto.toUpperCase() != "EROGACIONES CORRIENTES"){
+    	          $("#tbody-gastos-capital").append('<tr class="nivel-1"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	        }
+    	      }
+    	      if (partida_splited[0] == "0"){
+    	        $("#tbody-gastos-capital").append('<tr class="nivel-2"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	      }
+    	      if (partida_splited[0] == "1"){
+    	        $("#tbody-gastos-capital").append('<tr class="nivel-3"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	      }
+    	}
     });
     already_printed_capital=true;
 });
 }
 
 function dibujarD3_gastos_finalidad() {
-  $.getJSON("https://spreadsheets.google.com/feeds/list/1kNKGph0cxoVQf2WHSJK3ok7MoHg8vmISX5WFJ40jnyc/od6/public/values?alt=json", function( dataJSON ) {
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1kNKGph0cxoVQf2WHSJK3ok7MoHg8vmISX5WFJ40jnyc/values/"+selectedYear+"?key=AIzaSyDWqm99ehcgTUcnekuujkT2P95l-kor_mM", function( dataJSON ) {
     $("#gastos-finalidad").empty();
     var datos = [];
     var i = 0;
-    var entradas = dataJSON.feed.entry;
+    var entradas = dataJSON.values;
     $.each( entradas, function( key, val ) {
-      i += 1;
-
-      var concepto = val.gsx$clasificaciónporfinalidadyfunción.$t;
-      var total = val.gsx$total.$t;
-      if(i>1){
-        var linea = {"key": concepto,
-                "valor": parseInt(total.split('.').join(""))}
-        datos.push(linea);
-        if(already_printed_finalidad == false){
-          $("#tbody-gastos-finalidad").append('<tr class="nivel-3"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-        }
-      }else{
-        if(already_printed_finalidad == false){
-          $("#tbody-gastos-finalidad").append('<tr class="nivel-2"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-        }
-      }
+    	if(key>0){
+    	      i += 1;
+    	      var concepto = val[0];
+    	      var total = val[1];
+    	      if(i>1){
+    	        var linea = {"key": concepto,
+    	                "valor": parseInt(total.split('.').join(""))}
+    	        datos.push(linea);
+    	        if(already_printed_finalidad == false){
+    	          $("#tbody-gastos-finalidad").append('<tr class="nivel-3"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	        }
+    	      }else{
+    	        if(already_printed_finalidad == false){
+    	          $("#tbody-gastos-finalidad").append('<tr class="nivel-2"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	        }
+    	      }
+    	}
 
     });
     // Grafico Gastos por Clasificacion Economica
@@ -240,45 +247,48 @@ function dibujarD3_gastos_finalidad() {
 }
 
 function dibujarD3_gastos_finalidad_funcion() {
-  $.getJSON("https://spreadsheets.google.com/feeds/list/1kneGr7WYlVWQ4KyzH79bm3W5tHW4C45z1HYg1AYrlJg/od6/public/values?alt=json", function( dataJSON ) {
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1kneGr7WYlVWQ4KyzH79bm3W5tHW4C45z1HYg1AYrlJg/values/"+selectedYear+"?key=AIzaSyDWqm99ehcgTUcnekuujkT2P95l-kor_mM", function( dataJSON ) {
     $("#finalidadFuncionGraph").empty();
     var datos = [];
     var detalle = [];
     var i = 0;
     // console.log(dataJSON.feed.entry);
-    $.each( dataJSON.feed.entry, function( key, val ) {
-      i += 1;
-      var partida = val.gsx$partida.$t;
-      var partida_splited = partida.split('.');
-      var nivel = partida_splited.length;
-      var nivel_princ = parseInt(partida_splited[0]);
-      var concepto = val.gsx$clasificaciónporfinalidadyfunción.$t;
-      var total = val.gsx$total.$t;
-      var nivel_aux = nivel;
-      if (partida_splited[1]==""){
-        nivel -=1;
-      }else{
-        nivel_aux+=1;
-      }
-      if(already_printed_finalidad_funcion == false){
-        if(i>1){
-          $("#tbody-gastos-finalidad-funcion").append('<tr class="nivel-'+nivel_aux+'"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-        }else{
-          $("#tbody-gastos-finalidad-funcion").append('<tr class="nivel-1"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
-        }
-      }
+    $.each( dataJSON.values, function( key, val ) {
+    	if(key>0){
+    		 i += 1;
+    	      var partida = val[0];
+    	      var partida_splited = partida.split('.');
+    	      var nivel = partida_splited.length;
+    	      var nivel_princ = parseInt(partida_splited[0]);
+    	      var concepto = val[1];
+    	      var total = val[2];
+    	      var nivel_aux = nivel;
+    	      if (partida_splited[1]==""){
+    	        nivel -=1;
+    	      }else{
+    	        nivel_aux+=1;
+    	      }
+    	      if(already_printed_finalidad_funcion == false){
+    	        if(i>1){
+    	          $("#tbody-gastos-finalidad-funcion").append('<tr class="nivel-'+nivel_aux+'"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	        }else{
+    	          $("#tbody-gastos-finalidad-funcion").append('<tr class="nivel-1"><td>'+concepto+'</td><td>$'+total.toLocaleString("es-AR")+'</td></tr>');
+    	        }
+    	      }
 
-      if(partida_splited[0]!= "-1"){
-        detalle[nivel] = concepto;
-        if (nivel == 2){
-          var linea = {"key": detalle[2],
-                  "rec1": detalle[1],
-                 "valor": parseInt(total.split('.').join(""))
-               }
-            datos.push(linea);
-        }
+    	      if(partida_splited[0]!= "-1"){
+    	        detalle[nivel] = concepto;
+    	        if (nivel == 2){
+    	          var linea = {"key": detalle[2],
+    	                  "rec1": detalle[1],
+    	                 "valor": parseInt(total.split('.').join(""))
+    	               }
+    	            datos.push(linea);
+    	        }
 
-      }
+    	      }
+    	}
+     
 
     });
     // console.log(datos);
